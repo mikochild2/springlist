9a421ce3cf7e9bafcbd25dfdce353dd412301652
mikochild2 <mikochild2@gmail.com>
2020-06-22 11:28:06 -0400

    finishing touches. form issue: change methods to method so form will work

--- src/main/java/com/win/subscriberlist/subscriber/SubscriberController.java
+++ src/main/java/com/win/subscriberlist/subscriber/SubscriberController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 //Designates this class as a Controller
 @Controller
@@ -11,7 +12,7 @@ public class SubscriberController {
 
     // annotation that tells Spring to connect that info (subscriberrepo in this case) to this class
     @Autowired
-    private SubscriberRepository SubscriberRepository;
+    private SubscriberRepository subscriberRepository;
 
     //Tells our application what template to return at a specific URL
     @GetMapping
@@ -24,8 +25,9 @@ public class SubscriberController {
 
     //Takes the data entered in the form and adds it to the database.
     // POSTs the data then displays a new template called "result"
+    @PostMapping
     public String addNewSubscriber(Subscriber subscriber, Model model) {
-        SubscriberRepository.save(new Subscriber(subscriber.getFirstName(), subscriber.getLastName(), subscriber.getUserName(), subscriber.getSignedUp()));
+        subscriberRepository.save(new Subscriber(subscriber.getFirstName(), subscriber.getLastName(), subscriber.getUserName(), subscriber.getSignedUp()));
         model.addAttribute("firstName", subscriber.getFirstName());
         model.addAttribute("lastName", subscriber.getLastName());
         model.addAttribute("userName", subscriber.getUserName());
